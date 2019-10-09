aecbe6639169c61445a3ccda42dc8bd33cefce46
kovidomi <kovidomi01@citromail.hu>
2019-10-09 15:04:24 +0200

    Get cities in home component and list them

--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {WeatherService} from '../../../core/services/weather.service';
 import {Weather} from '../../../core/models/weather.model';
+import {CityReaderService} from '../../../core/services/city-reader.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,19 @@ import {Weather} from '../../../core/models/weather.model';
 })
 export class HomeComponent implements OnInit {
   currentWeather: Weather;
+  europeanCapitals: string[] = [];
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService, private cityReader: CityReaderService) {}
 
   ngOnInit() {
-    this.weatherService.getWeather().subscribe((data: Weather) => {
+    this.weatherService.getWeather('Budapest').subscribe((data: Weather) => {
       console.log(data);
       this.currentWeather = data;
+    });
+
+    this.cityReader.getEuropeanCities().subscribe((data) => {
+      this.europeanCapitals = data.split(/[\r\n]+/).sort();
+      console.log(this.europeanCapitals);
     });
   }
 
