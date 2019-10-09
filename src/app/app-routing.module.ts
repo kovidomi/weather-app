f10443cb128a673bc853e8b6aee78b6e7ca3193a
kovidomi <kovidomi01@citromail.hu>
2019-10-09 12:18:06 +0200

    Import routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import {ContentLayoutComponent} from './layouts/content-layout/content-layout.component';
+import { CONTENT_ROUTES } from './shared/routes/content-layout.routes';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: CONTENT_ROUTES
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
