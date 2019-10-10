e357af25b925ba6b7de93afb56aa1244da97ab76
kovidomi <kovidomi01@citromail.hu>
2019-10-10 06:32:23 +0200

    Add FontAwesome dependency

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import {MaterialModule} from './material.module';
-
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [],
   imports: [
-    MaterialModule
+    MaterialModule,
+    FontAwesomeModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    FontAwesomeModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
