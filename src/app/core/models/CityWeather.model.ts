49573b2b69a37fdf0d47178317ee7d6a677d0237
kovidomi <kovidomi01@citromail.hu>
2019-10-10 06:27:25 +0200

    Change order of fields

--- src/app/core/models/CityWeather.model.ts
+++ src/app/core/models/CityWeather.model.ts
@@ -1,16 +1,16 @@
 export class CityWeather {
+  public id: string;
+  public name: string;
   public coord: string;
+  public dt: string;
   public weather: string;
-  public base: string;
   public main: string;
+  public base: string;
   public wind: string;
   public clouds: string;
   public rain: string;
   public snow: string;
-  public dt: string;
   public sys: string;
   public timezone: string;
-  public id: string;
-  public name: string;
   public cod: string;
 }
